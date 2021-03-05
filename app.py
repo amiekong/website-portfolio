46b1cdb324490bf8996d66666d3fd669bc1a2cbe
amiekong <kong.am@husky.neu.edu>
2021-03-04 18:40:53 -0800

    added gifs and visuals to about me page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Blueprint, Flask, g, jsonify, render_template, request, Response, redirect, url_for, send_from_directory
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+import os
 
 # Configure application
 app = Flask(__name__)
@@ -6,8 +8,25 @@ app = Flask(__name__)
 @app.route('/index', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    title_text = 'Hi I’m Amie. I enjoy programming, creating, teaching, and giving back.'
+    return render_template('index.html',
+                                title_text=title_text,
+                                title="Data Science for Community",
+                                id="index")
 
-    greeting = 'Data Science for the Community'
 
-    return render_template('/index.html',
-                            greeting=greeting)
+@app.route('/about', methods=['POST', 'GET'])
+def about():
+    title_text = 'I took the Dental Admission Test, received a decent score, and with that I applied to a masters program in Computer Science...'
+    return render_template('/about.html',
+                            title_text=title_text,
+                            title="ABOUT ME",
+                            id="about")
+
+@app.route('/portfolio', methods=['POST', 'GET'])
+def portfolio():
+    title_text = 'Python (scikit-learn, NumPy, SciPy, Pandas, Keras, Gensim, Plotly), Java, SQL, HTML, CSS, Git, Unix, Flask, Data Extraction, Data Cleaning & Data Visualization'
+    return render_template('/portfolio.html',
+                            title_text=title_text,
+                            title="PROJECT PORTFOLIO",
+                            id="portfolio")
